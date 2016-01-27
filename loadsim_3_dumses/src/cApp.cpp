a4f2bfe51cf4397e9a105f6a78810316647c72ff
stdmtb <matobahiroshi@gmail.com>
2016-01-28 00:38:30 +0100

    ramses update

--- loadsim_3_dumses/src/cApp.cpp
+++ loadsim_3_dumses/src/cApp.cpp
@@ -1,3 +1,5 @@
+#define RENDER
+
 #include "cinder/app/AppNative.h"
 #include "cinder/Rand.h"
 #include "cinder/Utilities.h"
@@ -8,7 +10,6 @@
 #include "cinder/MayaCamUI.h"
 #include "cinder/Perlin.h"
 #include "cinder/params/Params.h"
-#include "CinderOpenCv.h"
 #include "mtUtil.h"
 #include "ConsoleColor.h"
 #include "Exporter.h"
@@ -30,6 +31,8 @@ public:
     void mouseDrag( MouseEvent event );
     void keyDown( KeyEvent event );
     void resize();
+
+    void loadPlotData( string fileName );
     void loadSimulationData( string fileName );
     
     int mWin_w = 1920;
@@ -41,201 +44,212 @@ public:
     
     Exporter mExp;
     int boxelx, boxely, boxelz;
+    
+    vector<double> pR, pTheta;
+    
+    int frame = 100;
+
+    string simType = "simu_1";
+    string log = "log";
+    string prm = "rho";
+
 };
 
 void cApp::setup(){
     setWindowPos( 0, 0 );
     setWindowSize( 1080*3*0.5, 1920*0.5 );
-    mExp.setup( 1080*3, 1920, 1, GL_RGB, mt::getRenderPath(), 0);
+    mExp.setup( 1080*3, 1920, 0, 3000, GL_RGB, mt::getRenderPath(), 0);
     
-    CameraPersp cam(1080*3, 1920, 54.4f, 1, 100000 );
-    cam.lookAt( Vec3f(0,0,600), Vec3f(0,0,0) );
+    CameraPersp cam(1080*3, 1920, 60.0f, 1, 100000 );
+    cam.lookAt( Vec3f(0,0,200), Vec3f(0,0,0) );
     cam.setCenterOfInterestPoint( Vec3f(0,0,0) );
-    //cam.setPerspective( 54.4f, getWindowAspectRatio(), 1, 100000 );     // 35mm
     camUi.setCurrentCam( cam );
     
     mPln.setSeed(123);
     mPln.setOctaves(4);
     
-    boxelx = boxely = boxelz = 400;
+    loadPlotData( "sim/supernova/plot/" + simType);
     
-    if( 1 ) loadSimulationData( "sim/Dumses/test/simu_2_rho_p_300f.bin" );
+    boxelx = pR.size();
+    boxely = pTheta.size();
+    boxelz = 1;
+    
+#ifdef RENDER
+    mExp.startRender();
+#endif
     
 }
+
+void cApp::loadPlotData(string simu_name){
+    
+    // load .r, .thata
+    vector<string> exts = { ".r", ".theta"};
+    for( auto & ext : exts ){
+    
+        fs::path assetPath = mt::getAssetPath();
+        string path = (assetPath/(simu_name + ext)).string();
+        cout << "start loading " << ext << " file : " << path << "...";
+        std::ifstream is( path, std::ios::binary );
+        if(is){ cout << " done" << endl;
+        }else{ cout << " ERROR" << endl; quit(); }
+        
+        is.seekg (0, is.end);
+        int fileSize = is.tellg();
+        is.seekg (0, is.beg);
+        int arraySize = arraySize = fileSize / sizeof(double);
+
+        vector<double> & vec = (ext == ".r")? pR : pTheta;
+        vec.clear();
+        vec.assign(arraySize, double(0));
+        is.read(reinterpret_cast<char*>(&vec[0]), fileSize);
+        is.close();
+        
+        printf( "arraySize %d, %e~%e\n\n", (int)vec.size(), vec[0], vec[vec.size()-1]);
+    }
+    
+    
+}
+
 
 void cApp::loadSimulationData(string fileName){
 
     fs::path assetPath = mt::getAssetPath();
     string path = (assetPath/fileName).string();
-    cout << "loading binary file : " << path << endl;
+    //cout << "start loading binary file : " << path << "...";
     std::ifstream is( path, std::ios::binary );
     if(is){
-        ccout::b("load OK binary file");
+       // cout << " done" << endl;
     }else{
-        ccout::r("load ERROR bin file");
+        cout << " ERROR" << endl;
         quit();
     }
     
     // get length of file:
     is.seekg (0, is.end);
     int fileSize = is.tellg();
-    cout << "length : " << fileSize << " byte" << endl;
     is.seekg (0, is.beg);
     
     int arraySize = arraySize = fileSize / sizeof(double);
-    cout << "arraySize : " << arraySize << endl;
+    //cout << "arraySize : " << arraySize << endl;
     
     vector<double> rho;
     rho.assign(arraySize, double(0));
     is.read(reinterpret_cast<char*>(&rho[0]), fileSize);
     is.close();
     
+    //cout << "Making point data... " << endl;
     
-    cout << "close binary file " << endl;
-    cout << "Making point data... " << endl;
-    
-    double in_min = std::numeric_limits<double>::max();
-    double in_max = std::numeric_limits<double>::min();
+    double in_min =  numeric_limits<double>::max();
+    double in_max =  numeric_limits<double>::min();
     
     for( auto r : rho ){
         in_min = MIN( in_min, r);
         in_max = MAX( in_max, r);
     }
-
-    cout << "min : " << in_min << endl;
-    cout << "max : " << in_max << endl;
-
+    
     vector<Vec3f> points;
     vector<ColorAf> colors;
     
-    for( int i=0; i<boxelx; i++ ){
-        for( int j=0; j<boxely; j++ ){
-            for( int k=0; k<boxelx; k++ ){
+    double scale = 600.0;
+    double extrude = 900.0;
+    for( int j=0; j<boxely; j++ ){
+        for( int i=0; i<boxelx; i++ ){
+            
+            int index = j + i*boxely;
+            
+            double rho_raw = rho[index];
+            float rho_map = lmap(rho_raw, in_min, in_max, 0.0, 1.0);
+            float visible_thresh = 0.2f;
+            
+            if( visible_thresh<rho_map && rho_map <1.00 ){
+                rho_map = lmap( rho_map, visible_thresh, 1.0f, 0.8f, 0.0f);
+                ColorAf color(CM_HSV, rho_map, 0.8f, 0.8);
                 
-                int index = i + j*boxely + k*boxelz*boxely;
-
-                int dimension = 3;
-                switch ( dimension) {
-                    case 1:
-                    {
-                        double rho_raw = rho[index];
-                        float rhof;
-                        
-                        bool logarizm = true;
-                        if( logarizm ){
-                            double rho_map = lmap(rho_raw, in_min, in_max, 1.0, 10.0);
-                            rhof = log10(rho_map);
-                        }else{
-                            double rho_map = lmap(rho_raw, in_min, in_max, 0.0, 1.0);
-                            rhof = rho_map;
-                        }
-                        
-                        float visible_thresh = 0.005f;
-                        
-                        if( rhof>visible_thresh ){
-                            rhof = lmap( rhof, visible_thresh, 1.0f, 0.001f, 0.7f);
-                            
-                            Vec3f noise = mPln.dfBm(k, j, i);
-                            
-                            ColorAf color(1,1,1,rhof);
-                            points.push_back( Vec3f(k-200, j-200, i-200) + noise );
-                            colors.push_back( color );
-                        }
-                        break;
-                    }
-                        
-                    case 3:
-                    {
-                        double x = rho[ index*3 + 0];
-                        double y = rho[ index*3 + 1];
-                        double z = rho[ index*3 + 2];
-                        double x_map = lmap( x, in_min, in_max, 1.0, 10.0 );
-                        double y_map = lmap( y, in_min, in_max, 1.0, 10.0 );
-                        double z_map = lmap( z, in_min, in_max, 1.0, 10.0 );
-                        float xf = log10( x_map );
-                        float yf = log10( y_map );
-                        float zf = log10( z_map );
- 
-                        
-                        double ulen = sqrt(x*x + y*y + z*z);
-                        double ulen_map = lmap( ulen, in_min, in_max, 1.0, 10.0 );
-                        float ulenf = log10(ulen_map);
-                        float visible_thresh = 0.78f;
-                        
-                        if( ulenf>visible_thresh ){
-                            ulenf = lmap( ulenf, visible_thresh, 1.0f, 0.000001f, 0.2f);
-                            
-                            Vec3f noise = mPln.dfBm(k, j, i) * 1.5;
-                            ColorAf color( xf, yf, zf, ulenf);
-                            points.push_back( Vec3f(k-200, j-200, i-200) + noise );
-                            colors.push_back( color );
-                        }
-                        break;
-                    }
+                bool polar = true;
+                if( polar ){
+                    double r = pR[i];
+                    double theta = pTheta[j];
+                    double x = r * cos( theta );
+                    double y = r * sin( theta );
+                    
+                    points.push_back( Vec3f( x*scale, y*scale, -rho_map*extrude) );
+                }else{
+                    points.push_back( Vec3f(i, j, 0) * scale);
                 }
+                colors.push_back( color );
             }
         }
     }
-    
-    
+
+
     gl::VboMesh::Layout layout;
     layout.setStaticColorsRGBA();
     layout.setStaticPositions();
-    
+
+    vbo.reset();
     vbo = gl::VboMesh(points.size(), 0, layout, GL_POINTS);
     vbo.bufferPositions(points);
     vbo.bufferColorsRGBA(colors);
     
-    cout << "create VBO : " << vbo.getNumVertices() << " verts" << endl;
+    //cout << "create VBO : " << vbo.getNumVertices() << " verts" << endl;
     cout << "Visible particle rate : " << vbo.getNumVertices()/(float)arraySize*100.0 << " %" << endl;
     
 }
 
 void cApp::update(){
+    
+    fs::path p("sim");
+    p = p/"supernova"/simType/log/prm/(simType + "_polar_" +log+ "_" + prm + "_00" + to_string(frame) + ".bin");
+    loadSimulationData(  p.string() );
+
 }
 
 void cApp::draw(){
 
-    mExp.begin( camUi.getCamera() );{
+    
+    gl::enableAlphaBlending();
+    gl::enableDepthRead();
+    gl::enableDepthWrite();
+    glPointSize(1);
+    glLineWidth(1);
+    
+    
+    //mExp.begin( camUi.getCamera() );{
+    mExp.beginOrtho();{
+        
         gl::clear( ColorA(0,0,0,1) );
-        gl::enableAlphaBlending();
         
-        if( !mExp.bSnap && !mExp.bRender ){
-            // Guide
-            mt::drawCoordinate(10);
+        //gl::translate(mExp.mFbo.getWidth()/2, mExp.mFbo.getHeight()/2);
+        gl::rotate(Vec3f(-90,0,0));
         
-            // base rect
-//            gl::lineWidth(1);
-//            gl::color(0.25, 0.25, 0.25);
-//            gl::pushModelView();{
-//                gl::translate(Vec3f(210,0,0));
-//                gl::rotate(Vec3f(0,90,0));
-//                gl::drawStrokedRect( Rectf( -200,200, 200,-200) );
-//            }gl::popModelView();
-        }
+        if( !mExp.bSnap && !mExp.bRender )
+            mt::drawCoordinate(1000);
         
-        {
-            // data
-            if(vbo){
-                glPointSize(1);
-                gl::draw(vbo);
-            }
+        if(vbo){
+            gl::draw(vbo);
+            gl::translate(0.5f, 0.5f);
+            gl::draw(vbo);
         }
         
 
     }mExp.end();
     
-    gl::clear( ColorA(1,1,1,1) );
-    gl::color( Color::white() );
     mExp.draw();
+    
+    gl::pushMatrices();
+    gl::setMatricesWindow(getWindowSize() );
+    gl::drawSolidRect(Rectf(Vec2i(0,0), Vec2i(120,50)));
+    gl::color(1, 1, 1);
+    gl::drawString("frame " + to_string(frame), Vec2f(20,20));
+    gl::popMatrices();
+    
+    frame++;
 }
 
 void cApp::keyDown( KeyEvent event ) {
     char key = event.getChar();
     switch (key) {
-        case 'S':
-            mExp.snapShot();
-            break;
+        case 'S': mExp.snapShot();  break;
     }
 }
 
